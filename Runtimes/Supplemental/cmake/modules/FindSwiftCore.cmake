b2aa9a51ca6bc7eeb6b419578d08c5b2a889182f
Eric Miotto <emiotto@apple.com>
2025-05-28 12:38:52 -0700

    More changes, tested under macOS

--- Runtimes/Supplemental/cmake/modules/FindSwiftCore.cmake
+++ Runtimes/Supplemental/cmake/modules/FindSwiftCore.cmake
@@ -32,7 +32,7 @@ The module may set the following variables if `SwiftCore_DIR` is not set.
  ``SwiftCore_FOUND``
    true if core was found
 
- ``SwiftCore_Core_INCLUDE_DIR``
+ ``SwiftCore_INCLUDE_DIR``
    the directory containing the Swift.swiftmodule folder
 
 #]=======================================================================]
@@ -55,156 +55,158 @@ include(FindPackageHandleStandardArgs)
 if(APPLE)
   # When building for Apple platforms, SwiftCore always comes from within the
   # SDK as a tbd for a shared library in the shared cache.
-  find_path(SwiftCore_Core_INCLUDE_DIR
+  find_path(SwiftCore_INCLUDE_DIR
       "Swift.swiftmodule"
     HINTS
       "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
-  find_library(SwiftCore_Core_IMPLIB
+  find_library(SwiftCore_IMPLIB
     NAMES "libswiftCore.tbd"
     HINTS
       "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
   add_library(swiftCore SHARED IMPORTED GLOBAL)
   set_target_properties(swiftCore PROPERTIES
-    IMPORTED_IMPLIB "${SwiftCore_Core_IMPLIB}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Core_INCLUDE_DIR}")
+    IMPORTED_IMPLIB "${SwiftCore_IMPLIB}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_INCLUDE_DIR}")
 
-  find_path(SwiftCore_Shims_INCLUDE_DIR
-      "module.modulemap"
-    HINTS
-      "${CMAKE_OSX_SYSROOT}/usr/lib/swift/shims")
-  add_library(swiftShims INTERFACE IMPORTED GLOBAL)
-  set_target_properties(swiftShims PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Shims_INCLUDE_DIR}")
-  target_link_libraries(swiftCore
-    PUBLIC
-    SwiftShims)
-
-  find_path(SwiftCoreOnoneSupport_INCLUDE_DIR
-      "OnoneSupport.swiftmodule"
+  find_path(Shims_INCLUDE_DIR
+      "shims/module.modulemap"
     HINTS
       "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
-  find_library(SwiftCoreOnoneSupport_IMPLIB
-    NAMES "libswiftOnoneSupport.tbd"
+  add_library(swiftShims INTERFACE IMPORTED GLOBAL)
+  set_target_properties(swiftShims PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${Shims_INCLUDE_DIR}/shims")
+  target_link_libraries(swiftCore
+   INTERFACE
+    swiftShims)
+
+  find_path(SwiftOnoneSupport_INCLUDE_DIR
+      "SwiftOnoneSupport.swiftmodule"
+    HINTS
+      "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
+  find_library(SwiftOnoneSupport_IMPLIB
+    NAMES "libswiftSwiftOnoneSupport.tbd"
     HINTS
       "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
   add_library(swiftOnoneSupport SHARED IMPORTED GLOBAL)
   set_target_properties(swiftOnoneSupport PROPERTIES
-    IMPORTED_IMPLIB "${SwiftCoreOnoneSupport_IMPLIB}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCoreOnoneSupport_INCLUDE_DIR}")
+    IMPORTED_IMPLIB "${SwiftOnoneSupport_IMPLIB}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftOnoneSupport_INCLUDE_DIR}")
 
-  find_path(SwiftCore_Concurrency_INCLUDE_DIR
+  find_path(SwiftConcurrency_INCLUDE_DIR
       "_Concurrency.swiftmodule"
     HINTS
       "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
-  find_library(SwiftCore_Concurrency_IMPLIB
+  find_library(SwiftConcurrency_IMPLIB
     NAMES "libswift_Concurrency.tbd"
     HINTS
       "${CMAKE_OSX_SYSROOT}/usr/lib/swift")
   add_library(swift_Concurrency SHARED IMPORTED GLOBAL)
   set_target_properties(swift_Concurrency PROPERTIES
-    IMPORTED_IMPLIB "${SwiftCore_Concurrency_IMPLIB}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Concurrency_INCLUDE_DIR}")
+    IMPORTED_IMPLIB "${SwiftConcurrency_IMPLIB}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftConcurrency_INCLUDE_DIR}")
 
   find_package_handle_standard_args(SwiftCore DEFAULT_MSG
-    SwiftCore_Core_IMPLIB SwiftCore_Core_INCLUDE_DIR
-    SwiftCore_Shims_INCLUDE_DIR
-    SwiftCore_OnoneSupport_IMPLIB SwiftCore_OnoneSupport_INCLUDE_DIR
-    SwiftCore_Concurrency_IMPLIB SwiftCore_Concurrency_INCLUDE_DIR)
+    SwiftCore_IMPLIB SwiftCore_INCLUDE_DIR
+    Shims_INCLUDE_DIR
+    SwiftOnoneSupport_IMPLIB SwiftOnoneSupport_INCLUDE_DIR
+    SwiftConcurrency_IMPLIB SwiftConcurrency_INCLUDE_DIR)
 elseif(LINUX)
   if (NOT BUILD_SHARED_LIBS)
-    find_path(SwiftCore_Core_INCLUDE_DIR
+    find_path(SwiftCore_INCLUDE_DIR
       "Swift.swiftmodule"
       HINTS
         "${Swift_SDKROOT}/usr/lib/swift_static/linux-static")
-    find_library(SwiftCore_Core_LIBRARY
+    find_library(SwiftCore_LIBRARY
       NAMES "libswiftCore.a"
       HINTS "${Swift_SDKROOT}/usr/lib/swift_static/linux-static")
     add_library(swiftCore STATIC IMPORTED GLOBAL)
 
-    find_path(SwiftCore_Shims_INCLUDE_DIR
-        "module.modulemap"
+    find_path(Shims_INCLUDE_DIR
+        "shims/module.modulemap"
         HINTS
-        "${Swift_SDKROOT}/usr/lib/swift/shims")
+        "${Swift_SDKROOT}/usr/lib/swift")
     add_library(swiftShims INTERFACE IMPORTED GLOBAL)
 
-    find_path(SwiftCore_OnoneSupport_INCLUDE_DIR
+    find_path(SwiftOnoneSupport_INCLUDE_DIR
       "SwiftOnoneSupport.swiftmodule"
       HINTS
         "${Swift_SDKROOT}/usr/lib/swift_static/linux-static")
-    find_library(SwiftCore_OnoneSupport_LIBRARY
+    find_library(SwiftOnoneSupport_LIBRARY
       NAMES "libswiftSwiftOnoneSupport.a"
       HINTS "${Swift_SDKROOT}/usr/lib/swift_static/linux-static")
     add_library(swiftOnoneSupport STATIC IMPORTED GLOBAL)
 
-    find_path(SwiftCore_Concurrency_INCLUDE_DIR
+    find_path(SwiftConcurrency_INCLUDE_DIR
       "_Concurrency.swiftmodule"
       HINTS
         "${Swift_SDKROO10GT}/usr/lib/swift_static/linux-static")
-    find_library(SwiftCore_Concurrency_LIBRARY
+    find_library(SwiftConcurrency_LIBRARY
       NAMES "libswift_Concurrency.a"
       HINTS "${Swift_SDKROOT}/usr/lib/swift_static/linux-static")
-    add_library(swiftConcurrency STATIC IMPORTED GLOBAL)
+    add_library(swift_Concurrency STATIC IMPORTED GLOBAL)
   else()
-    find_path(SwiftCore_Core_INCLUDE_DIR
+    find_path(SwiftCore_INCLUDE_DIR
       "Swift.swiftmodule"
       HINTS
         "${Swift_SDKROOT}/usr/lib/swift/linux")
-    find_library(SwiftCore_Core_LIBRARY
+    find_library(SwiftCore_LIBRARY
       NAMES "libswiftCore.so"
       HINTS "${Swift_SDKROOT}/usr/lib/swift/linux")
     add_library(swiftCore SHARED IMPORTED GLOBAL)
 
-    find_path(SwiftCore_Shims_INCLUDE_DIR
+    find_path(Shims_INCLUDE_DIR
         "module.modulemap"
         HINTS
         "${Swift_SDKROOT}/usr/lib/swift/shims")
     add_library(swiftShims INTERFACE IMPORTED GLOBAL)
 
-    find_path(SwiftCore_OnoneSupport_INCLUDE_DIR
+    find_path(SwiftOnoneSupport_INCLUDE_DIR
       "SwiftOnoneSupport.swiftmodule"
       HINTS
         "${Swift_SDKROOT}/usr/lib/swift/linux")
-    find_library(SwiftCore_OnoneSupport_LIBRARY
+    find_library(SwiftOnoneSupport_LIBRARY
       NAMES "libswiftSwiftOnoneSupport.so"
       HINTS "${Swift_SDKROOT}/usr/lib/swift/linux")
     add_library(swiftOnoneSupport STATIC IMPORTED GLOBAL)
 
-    find_path(SwiftCore_Concurrency_INCLUDE_DIR
+    find_path(SwiftConcurrency_INCLUDE_DIR
       "_Concurrency.swiftmodule"
       HINTS
         "${Swift_SDKROOT}/usr/lib/swift/linux")
-    find_library(SwiftCore_Concurrency_LIBRARY
+    find_library(SwiftConcurrency_LIBRARY
       NAMES "libswift_Concurrency.so"
       HINTS "${Swift_SDKROOT}/usr/lib/swift/linux")
-    add_library(swiftConcurrency STATIC IMPORTED GLOBAL)
+    add_library(swift_Concurrency STATIC IMPORTED GLOBAL)
   endif()
 
   set_target_properties(swiftCore PROPERTIES
-    IMPORTED_LOCATION "${SwiftCore_Core_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Core_INCLUDE_DIR}")
+    IMPORTED_LOCATION "${SwiftCore_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_INCLUDE_DIR}")
+  set_target_properties(swiftShims PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${Shims_INCLUDE_DIR}/shims")
   set_target_properties(swiftOnoneSupport PROPERTIES
-    IMPORTED_LOCATION "${SwiftCore_OnoneSupport_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_OnoneSupport_INCLUDE_DIR}")
-  set_target_properties(swiftConcurrency PROPERTIES
-    IMPORTED_LOCATION "${SwiftCore_Concurrency_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Concurrency_INCLUDE_DIR}")
+    IMPORTED_LOCATION "${SwiftOnoneSupport_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftOnoneSupport_INCLUDE_DIR}")
+  set_target_properties(swift_Concurrency PROPERTIES
+    IMPORTED_LOCATION "${SwiftConcurrency_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftConcurrency_INCLUDE_DIR}")
 
   target_link_libraries(swiftCore
-    PUBLIC
-    SwiftShims)
+    INTERFACE
+    swiftShims)
 
   find_package_handle_standard_args(SwiftCore DEFAULT_MSG
-    SwiftCore_Core_LIBRARY SwiftCore_Core_INCLUDE_DIR
-    SwiftCore_Shims_INCLUDE_DIR
-    SwiftCore_OnoneSupport_LIBRARY SwiftCore_OnoneSupport_INCLUDE_DIR
-    SwiftCore_Concurrency_LIBRARY SwiftCore_Concurrency_INCLUDE_DIR)
+    SwiftCore_LIBRARY SwiftCore_INCLUDE_DIR
+    Shims_INCLUDE_DIR
+    SwiftOnoneSupport_LIBRARY SwiftOnoneSupport_INCLUDE_DIR
+    SwiftConcurrency_LIBRARY SwiftConcurrency_INCLUDE_DIR)
 elseif(WIN32)
-  find_path(SwiftCore_Core_INCLUDE_DIR
+  find_path(SwiftCore_INCLUDE_DIR
     "Swift.swiftmodule"
     HINTS
       "${Swift_SDKROOT}/usr/lib/swift/windows"
       "$ENV{SDKROOT}/usr/lib/swift/windows")
-  find_library(SwiftCore_Core_LIBRARY
+  find_library(SwiftCore_LIBRARY
     NAMES "libswiftCore.lib"
     HINTS
       "${Swift_SDKROOT}/usr/lib/swift/${SwiftCore_PLATFORM_SUBDIR}/${SwiftCore_ARCH_SUBDIR}"
@@ -214,22 +216,27 @@ elseif(WIN32)
 
   add_library(swiftCore SHARED IMPORTED GLOBAL)
   set_target_properties(swiftCore PROPERTIES
-    IMPORTED_IMPLIB "${SwiftCore_Core_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Core_INCLUDE_DIR}")
+    IMPORTED_IMPLIB "${SwiftCore_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_INCLUDE_DIR}")
 
-  find_path(SwiftCore_Shims_INCLUDE_DIR
-    "module.modulemap"
+  find_path(Shims_INCLUDE_DIR
+    "shims/module.modulemap"
     HINTS
-      "${Swift_SDKROOT}/usr/lib/swift/shims"
-      "$ENV{SDKROOT}/usr/lib/swift/shims")
+      "${Swift_SDKROOT}/usr/lib/swift"
+      "$ENV{SDKROOT}/usr/lib/swift")
   add_library(swiftShims INTERFACE IMPORTED GLOBAL)
+  set_target_properties(swiftShims PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${Shims_INCLUDE_DIR}/shims")
+  target_link_libraries(swiftCore
+    INTERFACE
+    swiftShims)
 
-  find_path(SwiftCore_OnoneSupport_INCLUDE_DIR
+  find_path(SwiftOnoneSupport_INCLUDE_DIR
     "SwiftOnoneSupport.swiftmodule"
     HINTS
       "${Swift_SDKROOT}/usr/lib/swift/windows"
       "$ENV{SDKROOT}/usr/lib/swift/windows")
-  find_library(SwiftCore_OnoneSupport_LIBRARY
+  find_library(SwiftOnoneSupport_LIBRARY
     NAMES "libswiftSwiftOnoneSupport.lib"
     HINTS
       "${Swift_SDKROOT}/usr/lib/swift/${SwiftCore_PLATFORM_SUBDIR}/${SwiftCore_ARCH_SUBDIR}"
@@ -239,15 +246,15 @@ elseif(WIN32)
 
   add_library(swiftOnoneSupport SHARED IMPORTED GLOBAL)
   set_target_properties(swiftOnoneSupport PROPERTIES
-    IMPORTED_IMPLIB "${SwiftCore_OnoneSupport_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_OnoneSupport_INCLUDE_DIR}")
+    IMPORTED_IMPLIB "${SwiftOnoneSupport_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftOnoneSupport_INCLUDE_DIR}")
 
-  find_path(SwiftCore_Concurrency_INCLUDE_DIR
+  find_path(SwiftConcurrency_INCLUDE_DIR
     "Concurrency.swiftmodule"
     HINTS
       "${Swift_SDKROOT}/usr/lib/swift/windows"
       "$ENV{SDKROOT}/usr/lib/swift/windows")
-  find_library(SwiftCore_Concurrency_LIBRARY
+  find_library(SwiftConcurrency_LIBRARY
     NAMES "libswift_Concurrency.lib"
     HINTS
       "${Swift_SDKROOT}/usr/lib/swift/${SwiftCore_PLATFORM_SUBDIR}/${SwiftCore_ARCH_SUBDIR}"
@@ -255,16 +262,16 @@ elseif(WIN32)
       "$ENV{SDKROOT}/usr/lib/swift/${SwiftCore_PLATFORM_SUBDIR}/${SwiftCore_ARCH_SUBDIR}"
       "$ENV{SDKROOT}/usr/lib/swift")
 
-  add_library(swiftConcurrency SHARED IMPORTED GLOBAL)
-  set_target_properties(swiftConcurrency PROPERTIES
-    IMPORTED_IMPLIB "${SwiftCore_Concurrency_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${SwiftCore_Concurrency_INCLUDE_DIR}")
+  add_library(swift_Concurrency SHARED IMPORTED GLOBAL)
+  set_target_properties(swift_Concurrency PROPERTIES
+    IMPORTED_IMPLIB "${SwiftConcurrency_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SwiftConcurrency_INCLUDE_DIR}")
 
   find_package_handle_standard_args(SwiftCore DEFAULT_MSG
-    SwiftCore_Core_LIBRARY SwiftCore_Core_INCLUDE_DIR
-    SwiftCore_Shims_INCLUDE_DIR
-    SwiftCore_OnoneSupport_IMPLIB SwiftCore_OnoneSupport_INCLUDE_DIR
-    SwiftCore_Concurrency_IMPLIB SwiftCore_Concurrency_INCLUDE_DIR)
+    SwiftCore_LIBRARY SwiftCore_INCLUDE_DIR
+    Shims_INCLUDE_DIR
+    SwiftOnoneSupport_IMPLIB SwiftOnoneSupport_INCLUDE_DIR
+    SwiftConcurrency_IMPLIB SwiftConcurrency_INCLUDE_DIR)
 else()
   message(FATAL_ERROR "FindSwiftCore.cmake module search not implemented for targeted platform\n"
   " Build Core for your platform and set `SwiftCore_DIR` to"
